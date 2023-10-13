f44e0d3bf3af0bc9ccfd8c12fd24c36785b1d6f4
ajCameron <alexanderjfcameron@gmail.com>
2023-10-13 01:45:52 +0100

    Not entirely sure what's going wrong with the render code there

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setuptools.setup(
 
     python_requires=">=3.10",  # Might be relaxed later
 
-    version=get_version('src/mewbot/io/discord'),
+    version=get_version('src/mewbot/io/discord/__init__.py'),
 
     install_requires=requirements,
 
@@ -128,18 +128,6 @@ setuptools.setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Operating System :: OS Independent',
-
-        { % if cookiecutter.license == "MIT" - %}
-        'License :: OSI Approved :: MIT License',
-        { % - elif cookiecutter.license == "BSD-3" - %}
-        'License :: OSI Approved :: BSD License',
-        { % - elif cookiecutter.license == "GNU GPL v3.0" - %}
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        { % - elif cookiecutter.license == "Apache Software License 2.0" - %}
-        'License :: OSI Approved :: Apache Software License',
-        { % - elif cookiecutter.license == "Mozilla Public License 2.0" - %}
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-        { % - endif %}
 
     ],
 
